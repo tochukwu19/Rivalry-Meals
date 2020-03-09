d7202fd81cf45ee41e2f513e10904c891993f8f1
Tochukwu Onwunali <onwunali91@yahoo.com>
2020-03-09 13:46:59 +0000

    Nav bar done

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+const
+    ham = document.querySelector(".hamburger"),
+    ham3 = document.querySelector(".ham3"),
+    ham2 = document.querySelector(".ham2"),
+    ham1 = document.querySelector(".ham1");
+
+
+// Hamburger JS
+const menuOpen = e => {
+    ham1.classList.toggle("ham-tilt-right");
+    ham2.classList.toggle("ham-no-show");
+    ham3.classList.toggle("ham-tilt-left");
+    navbar.classList.toggle("nav-out");
+}
+ham.addEventListener("click", menuOpen)
