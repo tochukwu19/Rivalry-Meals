0ae324cf79f428dbd04de516bf323700a210cd43
Tochukwu Onwunali <onwunali91@yahoo.com>
2020-03-18 13:36:37 +0000

    Done cleaning up

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
-
 const
     ham = document.querySelector(".hamburger"),
     ham3 = document.querySelector(".ham3"),
     ham2 = document.querySelector(".ham2"),
     ham1 = document.querySelector(".ham1"),
-    nav = document.querySelector(".nav-out");
-
+    nav = document.querySelector(".nav-out"),
+    body = document.querySelector("body"),
+    stuffs = document.querySelectorAll(".stuff")
 // Hamburger JS
 
 
@@ -14,7 +14,18 @@ const menuOpen = e => {
     ham2.classList.toggle("ham-no-show");
     ham3.classList.toggle("ham-tilt-left");
     navbar.classList.toggle("nav-out");
-    ham.classList.toggle("abspos")
+    body.classList.toggle("overflowy");
 }
 
 ham.addEventListener("click", menuOpen);
+
+[...stuffs].forEach(stuff => {
+    stuff.addEventListener('click', () => {
+        navbar.classList.remove("nav-out");
+        ham1.classList.remove("ham-tilt-right");
+        ham2.classList.remove("ham-no-show");
+        ham3.classList.remove("ham-tilt-left");
+        body.classList.remove("overflowy");
+
+    });
+});
